0d2c55a9fdbe3ee7f657260013115e39204cfa01
chaosed0 <chaosed0@gmail.com>
2017-01-21 10:13:30 -0800

    Simple pathfinding

--- Assets/scripts/Level.cs
+++ Assets/scripts/Level.cs
@@ -15,8 +15,6 @@ public class Level : MonoBehaviour {
     public SpriteRenderer tilePrefab;
     public Vector2 tileSize;
 
-    public Camera camera;
-
     public Vector2 tileScale = new Vector2(2.0f, 2.0f);
     public float pixelsPerUnit = 108.0f;
 
@@ -44,13 +42,116 @@ public class Level : MonoBehaviour {
         }
 	}
 
-    public GameObject TileAtPosition(Vector2 mousePosition) {
-        Vector3 worldPoint = camera.ScreenToWorldPoint(mousePosition);
+    public GameObject TileAtPosition(Vector2 worldPoint) {
+        return TileAtTileCoords(WorldToTilePosition(worldPoint));
+    }
 
+    public Vector2[] FindPath(Vector2 start, Vector2 finish)
+    {
+        Vector2 startTile = WorldToTilePosition(start);
+        Vector2 finishTile = WorldToTilePosition(finish);
+
+        if (!isTilePassable(startTile)) {
+            return new Vector2[] { start };
+        }
+
+        List<Vector2> queue = new List<Vector2>();
+        queue.Add(startTile);
+
+        Dictionary<int, Vector2> previousVisited = new Dictionary<int, Vector2>();
+        previousVisited.Add(TilePositionToTileId(startTile), start);
+
+        while (queue.Count > 0) {
+            Vector2 tilePosition = queue[0];
+            queue.RemoveAt(0);
+
+            int tileX = (int)tilePosition.x;
+            int tileY = (int)tilePosition.y;
+
+            if (tileX == (int)finishTile.x &&
+                tileY == (int)finishTile.y) {
+                break;
+            }
+            
+            Vector2 rightTile = new Vector2(tileX + 1, tileY);
+            Vector2 leftTile = new Vector2(tileX - 1, tileY);
+            Vector2 downTile = new Vector2(tileX, tileY + 1);
+            Vector2 upTile = new Vector2(tileX, tileY - 1);
+
+            if (isTilePassable(rightTile) && !previousVisited.ContainsKey(TilePositionToTileId(rightTile))) {
+                queue.Add(rightTile);
+                previousVisited.Add(TilePositionToTileId(rightTile), tilePosition);
+            }
+            if (isTilePassable(leftTile) && !previousVisited.ContainsKey(TilePositionToTileId(leftTile))) {
+                queue.Add(leftTile);
+                previousVisited.Add(TilePositionToTileId(leftTile), tilePosition);
+            }
+            if (isTilePassable(downTile) && !previousVisited.ContainsKey(TilePositionToTileId(downTile))) {
+                queue.Add(downTile);
+                previousVisited.Add(TilePositionToTileId(downTile), tilePosition);
+            }
+            if (isTilePassable(upTile) && !previousVisited.ContainsKey(TilePositionToTileId(upTile))) {
+                queue.Add(upTile);
+                previousVisited.Add(TilePositionToTileId(upTile), tilePosition);
+            }
+        }
+
+        if (!previousVisited.ContainsKey(TilePositionToTileId(finishTile))) {
+            // Didn't find a path
+            return new Vector2[] { start };
+        }
+
+        // Reconstruct the path from the finish
+        List<Vector2> tilePath = new List<Vector2>();
+        tilePath.Add(finishTile);
+        Vector2 pathTile = finishTile;
+        while ((int)pathTile.x != (int)startTile.x || (int)pathTile.y != (int)startTile.y) {
+            bool gotten = previousVisited.TryGetValue(TilePositionToTileId(pathTile), out pathTile);
+            if (!gotten) {
+                // Error
+                Debug.LogError("We thought we found a path but we really didn't");
+                break;
+            }
+            tilePath.Add(pathTile);
+        }
+
+        // Reverse tilePath and convert to world coords
+        List<Vector2> worldPath = new List<Vector2>();
+        for (int i = tilePath.Count - 1; i >= 0; i--) {
+            worldPath.Add(TileToWorldPosition(tilePath[i]));
+        }
+        
+        return worldPath.ToArray();
+    }
+
+    private int TilePositionToTileId(Vector2 tilePosition) {
+        int tileX = (int)tilePosition.x;
+        int tileY = (int)tilePosition.y;
+        return tileY * levelMap.GetLength(1) + tileX;
+    }
+
+    private Vector2 WorldToTilePosition(Vector2 worldPoint) {
         float localX = (worldPoint.x - this.transform.position.x) * pixelsPerUnit;
         float localY = - (worldPoint.y - this.transform.position.y) * pixelsPerUnit;
-        int tileX = (int)Mathf.Floor(localX / (tileSize.x * tileScale.x));
-        int tileY = (int)Mathf.Floor(localY / (tileSize.y * tileScale.y));
+        float tileX = Mathf.Floor(localX / (tileSize.x * tileScale.x));
+        float tileY = Mathf.Floor(localY / (tileSize.y * tileScale.y));
+
+        return new Vector2(tileX, tileY);
+    }
+
+    // Note, returns position centered on tile
+    private Vector2 TileToWorldPosition(Vector2 tilePosition) {
+        float localX = (tilePosition.x + 0.5f) * tileSize.x * tileScale.x;
+        float localY = (tilePosition.y + 0.5f) * tileSize.y * tileScale.y;
+        float worldX = localX / pixelsPerUnit + this.transform.position.x;
+        float worldY = localY / pixelsPerUnit + this.transform.position.y;
+
+        return new Vector2(worldX, worldY);
+    }
+
+    private GameObject TileAtTileCoords(Vector2 tilePosition) {
+        int tileX = (int)tilePosition.x;
+        int tileY = (int)tilePosition.y;
 
         if (tileY >= 0 &&
             tileX >= 0 &&
@@ -61,8 +162,10 @@ public class Level : MonoBehaviour {
             return levelTiles[tileY, tileX].gameObject;
         }
 
-        Debug.Log(localX + " " + localY + " " + tileX + " " + tileY);
-
         return null;
+    }
+
+    private bool isTilePassable(Vector2 tilePosition) {
+        return TileAtTileCoords(tilePosition) != null;
     }
 }
--- Assets/scripts/PlayerInput.cs
+++ Assets/scripts/PlayerInput.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
     public Level level;
+    public Camera camera;
+
+    bool hasSelected = false;
+    private Vector2 selectedPosition;
 
 	void Start () {
 	}
@@ -13,13 +17,21 @@ public class PlayerInput : MonoBehaviour {
 
         if (click) {
             Vector3 mousePosition = Input.mousePosition;
+            Vector3 worldPoint = camera.ScreenToWorldPoint(mousePosition);
+
             if (mousePosition.x >= 0.0f && mousePosition.y >= 0.0f &&
                 mousePosition.x <= Screen.width && mousePosition.y <= Screen.height)
             {
-                GameObject tile = level.TileAtPosition(new Vector2(mousePosition.x, mousePosition.y));
-                Debug.Log(tile);
+                GameObject tile = level.TileAtPosition(new Vector2(worldPoint.x, worldPoint.y));
                 if (tile != null) {
-                    // Path
+                    if (hasSelected == false) {
+                        selectedPosition = worldPoint;
+                        hasSelected = true;
+                        Debug.Log(worldPoint);
+                    } else {
+                        hasSelected = false;
+                        Debug.Log(level.FindPath(selectedPosition, worldPoint));
+                    }
                 }
             }
         }
