531512abb7e6ad11ca7b4c1862250464ea4f4733
Albert Shih <albertbshih@gmail.com>
2017-01-21 16:00:57 -0800

    Added UI red dots

---
+++ Assets/scripts/Radar Tech/FadeOutAndDestroy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutAndDestroy : MonoBehaviour {
+
+	public float startAlpha = 1;
+
+	public float maxLifetime = 2;
+
+	float curLife;
+
+	SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+
+		curLife = maxLifetime;
+
+		sprite = GetComponent<SpriteRenderer> ();
+
+		StartCoroutine (Countdown());
+	}
+
+	IEnumerator Countdown()
+	{
+
+		while (curLife > 0) 
+		{
+			curLife -= Time.deltaTime;
+
+			Color c = sprite.color;
+
+			c.a = curLife / maxLifetime;
+
+			sprite.color = c;
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		Destroy (gameObject);
+
+
+	}
+}
